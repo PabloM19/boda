91335e931bf7eb46f7510ad88a9db39e6fdaa01f
PabloM19 <pmoleromarin@gmail.com>
2025-05-12 12:38:20 +0200

    niose

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
-export default {
-    root: '.',
-  };
-  
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        galeriavirtual: './galeriavirtual.html'
+      }
+    }
+  }
+})
